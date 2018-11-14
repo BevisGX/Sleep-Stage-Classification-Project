9fcdd3ced3e3b897a27fc24b9833d07a7864f658
xmk2222 <32953135+xmk2222@users.noreply.github.com>
2018-11-14 19:39:59 +0800

    Improve EDF loader

--- preprocessing/edfLoader.py
+++ preprocessing/edfLoader.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 
 """
-This script is used to extract features from the raw EEG signals
-Features will be stored in several npz file, and all labels will be stored in corresponding npz file
+This script is used to extract features and labels from the raw EEG signals captured by Alice A6
+Features are stored in .EDF files, and labels are stored in .xml(.rml) files.
+Features will be stored in several npz file, and all labels will be stored in corresponding csv file
 """
 
 import numpy as np
+import pandas as pd
 import pyedflib
 import sys, os, re
 import xml.etree.ElementTree as ET
@@ -16,11 +18,13 @@ from preprocessing.xmlTool import removePrefix, findSubelement
 ##################################
 def read_edf_file(file_path):
     """
-    read a raw edf file, and get the sampling data for the given channels
+    Read a raw edf file, and get the sampling data for the given channels
+
     input:
         file_path: the path to the edf file
     output:
-        eeg_raw: a k*N shape numpy array, where k (k=2) is the number of channels, N=Fs*T is the total number of samples for one patient, Fs is the sampling rate, T is the recording duration
+        eeg_raw: a k*N shape numpy array, where k is the number of channels, N=Fs*T is the total number of samples for
+            one patient, Fs is the sampling rate, T is the recording duration
         labels: channel names corresponding to the signals
         Fs: sampling rate of each channel
     """
@@ -46,8 +50,14 @@ def read_edf_file(file_path):
 #################################
 def extractRawFeature(dir_path, output_file):
     """
-    extract the raw eeg feature for each 30 second epoch, and output the generated features to a text file
-    The feature array outputed will be in shape of k*n*30Fs, where k is the number of channels, Fs is the samping rate, and n is the number of epoches. The feature arrays for all patients will be outputted to a npz file
+    Extract the raw eeg feature for each 30 second epoch, and output the generated features to .npz file
+
+    The feature and relative informations would be saved in .npy format and wrapped up as one .npz file
+    The keys of save dictionary include:
+        'channels' : a list of channel names
+        'eeg_raw' : a (k, n, 30*Fs) numpy array, where k is the number of channels, Fs is the sampling rate, and n is the number of epochs
+        'sampling rate': a list of sampling rate related to channel names
+
     input:
         dir_path: the full path of the directory which contains all edf files
         output_file: the path of the output file which stores all the features
@@ -64,7 +74,7 @@ def extractRawFeature(dir_path, output_file):
             if fname.split('.')[1] != 'edf' or "T" in fname:        #bad data, sad
                 continue
             fname = dir_path+'/'+dirName+'/'+fname
-            eeg_raw, labels, fss = read_edf_file(fname)
+            eeg_raw, channels, fss = read_edf_file(fname)
             fs = fss[0]         #as the sampling rates are the same
             print(eeg_raw.shape)
             k, M = eeg_raw.shape
@@ -77,7 +87,7 @@ def extractRawFeature(dir_path, output_file):
         for i in range(1, len(result)):
             eeg_raw = np.concatenate((eeg_raw, result[i]), axis = 1)        #concatenate signals orignally saved in different files to a complete signal
         save_dict = {
-            "channels": labels,
+            "channels": channels,
             "eeg_raw": eeg_raw,
             "sampling rate": fss
         }
@@ -91,11 +101,15 @@ def extractRawFeature(dir_path, output_file):
 #################################
 def extractLabels(dir_path,output_file):
     """
-    extract labels for all eeg epoches from given xml files, and output the labels to text file
-    The output matrix is in shape of N*n, where N is the number of patient, n is the number of epoches.
+    Extract sleep stage labels for all eeg epochs from given xml files, and output the labels to .npz file and it's
+    one-hot-encoding data to .csv file
+
+    The labels save to .npz would be in shape of (n), and in .csv file labels would be encoded by one-hot-encoding,
+    the output matrix is in shape of (n, 5), where n is the number of epochs.
+
     input:
-        dir_path: the directory which contains the xml files
-        output_file: the outputted file
+        dir_path: the string of directory which contains the xml files
+        output_file: the string of output file path
     output:
         None
     """
@@ -106,8 +120,13 @@ def extractLabels(dir_path,output_file):
                 'NonREM3': 3,
                 'REM': 5
                 }
-    result = []
-    row_indexer = 0
+    stageMapCsv = {-1: [1, 0, 0, 0, 0],
+                   0: [1, 0, 0, 0, 0],
+                   1: [0, 1, 0, 0, 0],
+                   2: [0, 0, 1, 0, 0],
+                   3: [0, 0, 0, 1, 0],
+                   5: [0, 0, 0, 0, 1]
+                   }
     num_files = 0
     for dirName in os.listdir(dir_path):
         nsrrid = dirName.split('-')[0]
@@ -137,6 +156,52 @@ def extractLabels(dir_path,output_file):
         save_dict = {
             "stage_labels": data
         }
-        np.savez(os.path.join(output_file, "Labels_"+nsrrid), **save_dict)
+        np.savez(os.path.join(output_file, "Labels_" + nsrrid), **save_dict)
+        one_hot_data = []
+        for i in range(len(data)):
+            one_hot_data.append(stageMapCsv[data[i]])
+        np.asarray(one_hot_data)
+        one_hot_data = pd.DataFrame(one_hot_data)
+        one_hot_data.to_csv(os.path.join(output_file, "Labels_"+nsrrid+'.csv'), header=False, index=False)
         num_files += 1
     print("extracted %d files for labels" % num_files)
+
+
+#################################
+# step 4: extract events
+#################################
+def extractEvents(dir_path,output_file):
+    """Extract sleep events information from .xml(rml) file
+
+    The events will be saved to .csv file, and its columns will be ['事件', '类型', '时间', '所在帧', '持续时间', '所在信号']
+
+    input:
+        dir_path: the string of directory which contains the xml files
+        output_file: the string of output file path
+    output:
+        None
+    """
+    num_files = 0
+    for dirName in os.listdir(dir_path):
+        nsrrid = dirName.split('-')[0]
+        # if num_files == 1: break
+        for fname in os.listdir(dir_path + '/' + dirName):
+            if fname.split('.')[1] != 'rml':
+                continue
+            fname = dir_path + '/' + dirName + '/' + fname
+            print(fname)
+            tree = ET.parse(fname)
+            root = tree.getroot()
+            removePrefix(root)
+            events = findSubelement(root, 'Events')
+            # duration = int(float(findSubelement(root, 'Duration').text))
+            data = pd.DataFrame(columns=['事件', '类型', '时间', '所在帧', '持续时间', '所在信号'])
+            for event in events.iterfind('Event'):
+                family = event.get('Family')
+                type_ = event.get('Type')
+                start = event.get('Start')
+                duration = event.get('Duration')
+                data = data.append({'事件': family, '类型': type_, '时间': start, '所在帧': None, '持续时间': duration, '所在信号': None}, ignore_index=True)
+        data.to_csv(os.path.join(output_file, "Events_" + nsrrid + '.csv'), index=False)
+        num_files += 1
+    print("extracted %d files for events" % num_files)
--- src/genRAW.py
+++ src/genRAW.py
@@ -6,7 +6,7 @@ Features will be stored in several npz file, and all labels will be stored in co
 You need to specify the paths to edf files and xml files, you also need to set the paths of the generated npz files
 """
 import os
-from preprocessing.edfLoader import extractLabels, extractRawFeature
+from preprocessing.edfLoader import extractLabels, extractRawFeature, extractEvents
 
 if __name__ == '__main__':
     dir_path = "E:\\Document\\sleep data\\A6_data_exp_part2"
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     if not os.path.exists(output_file):
         os.makedirs(output_file)
 
-    extractRawFeature(dir_path,output_file)
-    extractLabels(dir_path,output_file)
+    # extractRawFeature(dir_path,output_file)
+    # extractLabels(dir_path,output_file)
+    extractEvents(dir_path, output_file)
